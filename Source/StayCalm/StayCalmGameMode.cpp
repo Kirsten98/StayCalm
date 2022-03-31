093f3b69a6059bc6fc5d86876b6d96d2ba65f3bf
Kirsten98 <42506533+Kirsten98@users.noreply.github.com>
2022-03-30 20:35:06 -0700

    Initial Commit
    First commit to start the project. Includes updated test map and slight functionality update to Stay Calm Character.

---
+++ Source/StayCalm/StayCalmGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "StayCalmGameMode.h"
+#include "StayCalmHUD.h"
+#include "StayCalmCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AStayCalmGameMode::AStayCalmGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AStayCalmHUD::StaticClass();
+}
