9b194d2cbd0fb2942ea8d5896d3292f530d5deb7
Kirsten98 <42506533+Kirsten98@users.noreply.github.com>
2022-12-04 19:29:33 -0800

    Adds comments to Blueprint functions. Removes on screen debug logs.

--- Source/StayCalm/PanicTrigger.h
+++ Source/StayCalm/PanicTrigger.h
@@ -16,18 +16,22 @@ public:
 	// Sets default values for this actor's properties
 	APanicTrigger();
 
+	//Static Mesh for Trigger
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = mesh)
 		class UStaticMeshComponent* trigger_mesh;
 
+	//Material to display when the trigger is active
 	UPROPERTY(EditAnywhere)
 		class UMaterial* on_material;
 
+	//Material to display when the trigger is inactive
 	UPROPERTY(EditAnywhere)
 		class UMaterial* off_material;
 
 	//The panic level that should be caused by this object
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Panic);
 	int panic_level = 0;
+
 	inline int get_panic_level(){ return panic_level; };
 
 	UFUNCTION(BlueprintCallable)
@@ -46,39 +50,16 @@ public:
 	UFUNCTION(BlueprintImplementableEvent, Category = Panic)
 		void trigger_event();
 	
-	//Sets the comparison between panic triggers
-	/*
-	friend bool operator< (const APanicTrigger& l, const APanicTrigger& r)
-	{
-		return l.panic_level
-			< r.panic_level; // keep the same order
-	}
 
-	friend bool operator> (const APanicTrigger& l, const APanicTrigger& r)
-	{
-		return l.panic_level
-			> r.panic_level; // keep the same order
-	}
-
-	friend bool operator<= (const APanicTrigger& l, const APanicTrigger& r)
-	{
-		return l.panic_level
-			<= r.panic_level; // keep the same order
-	}
-
-	friend bool operator>=(const APanicTrigger& l, const APanicTrigger& r)
-	{
-		return l.panic_level
-			>= r.panic_level; // keep the same order
-	}
-	*/
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//Used to determine if the trigger is visible in game
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Panic)
 	bool is_visible = true;
 
+	//Used to determine if the trigger is active and can be triggered
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Panic)
 	bool panic_trigger_active = false;
 
